b63ef6dfe8e6dd1ca2349d1cb5bae7ed211c2c8f
Marllef H A Freitas <marllef-alves@hotmail.com>
2022-04-07 22:11:15 -0300

    fix: removendo parametros desnecessarios

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { PokeDataContext } from "~/contexts/DataContext";
 
 const useData = () => {
-  const { data, observerRef, ...rest } = useContext(PokeDataContext)!;
+  const { data, ...rest } = useContext(PokeDataContext)!;
 
   return {
     data,
-    observerRef,
+
     ...rest,
   };
 };
